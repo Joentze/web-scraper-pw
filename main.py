69266a3ace3e070029b5c75c99ea9a4e9595b8f6
Joentze <Joentze01@gmail.com>
2022-09-30 22:51:28 +0800

    feat: added config and parser files

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 from time import time
 from threading import Thread
 from playwright.sync_api import sync_playwright
+from config import MAX_NUM_OF_THREADS, PLAYWRIGHT_HEADLESS, PLAYWRIGHT_TIMEOUT, DEFAULT_SELECTOR_TO_WAIT
+from parser import parse_tags, parse_tags_by_class
 
-configs=[{
+configs=[
+    {
     "url":"https://www.channelnewsasia.com/?cid=google_sem_paid_12042022_cnamkt&gclid=CjwKCAjwp9qZBhBkEiwAsYFsb_9Ur0tbWTMgZ4dKxwPckd4rK2vLPs-zjeK1fX0K-31lxDvvAIy3wxoCBywQAvD_BwE"
 },{
     "url":"https://www.channelnewsasia.com/?cid=google_sem_paid_12042022_cnamkt&gclid=CjwKCAjwp9qZBhBkEiwAsYFsb_9Ur0tbWTMgZ4dKxwPckd4rK2vLPs-zjeK1fX0K-31lxDvvAIy3wxoCBywQAvD_BwE"
-}]
+},{
+    "url":"https://www.channelnewsasia.com/?cid=google_sem_paid_12042022_cnamkt&gclid=CjwKCAjwp9qZBhBkEiwAsYFsb_9Ur0tbWTMgZ4dKxwPckd4rK2vLPs-zjeK1fX0K-31lxDvvAIy3wxoCBywQAvD_BwE"
+},{
+    "url":"https://www.channelnewsasia.com/?cid=google_sem_paid_12042022_cnamkt&gclid=CjwKCAjwp9qZBhBkEiwAsYFsb_9Ur0tbWTMgZ4dKxwPckd4rK2vLPs-zjeK1fX0K-31lxDvvAIy3wxoCBywQAvD_BwE"
+},
+]
+
 
 
 def segment_content(content:list[object],num_of_threads:int)->None:
@@ -31,21 +40,29 @@ def get_inner_html(config:object)->str:
         w_sel = config["wait_for_selector"]
     #OPENS WEB DRIVER
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, timeout=10000)
+        browser = p.chromium.launch(headless=PLAYWRIGHT_HEADLESS, timeout=PLAYWRIGHT_TIMEOUT)
         page = browser.new_page(java_script_enabled=True)
         page.goto(config["url"])
         page.wait_for_selector(w_sel)
-        html = page.inner_html("html")
-    print(html)
+        html = page.inner_html(DEFAULT_SELECTOR_TO_WAIT)
+        #//{tag}[@{identifier}='{name}']
+        # print([i.get_attribute("href") for i in page.query_selector_all(f"//a[@class='h6__link list-object__heading-link']")])
+    # print(parse_tags_by_class("a","h6__link list-object__heading-link",html))
+
     return html 
+
+def get_segment_inner_html(segment:list[object])->None:
+    for config in segment:
+        get_inner_html(config)
 
 
 if __name__ == "__main__":
     start = time()
     
-    TOTAL_THREADS = 4
-    # [print(len(x)) for x in segment_content(configs, TOTAL_THREADS)]
-    threads = [Thread(target=get_inner_html,args=[configs[i]]) for i in range(TOTAL_THREADS)]
+    TOTAL_THREADS = MAX_NUM_OF_THREADS
+    
+    [print(len(x)) for x in segment_content(configs, TOTAL_THREADS)]
+    threads = [Thread(target=get_segment_inner_html,args=[x]) for x in segment_content(configs, TOTAL_THREADS)]
     #START THREADS
     for t in threads:
         t.start()
---
+++ parser.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+def parse_tags(tag:str,html:str)->list:
+    soup = BeautifulSoup(html)
+    return soup.find_all(tag)
+
+def parse_tags_by_class(tag:str, class_name:str, html:str)->list:
+    soup = BeautifulSoup(html)
+    return soup.find_all(tag, {"class":class_name})
+
+def get_attribute_by_class(attribute:str ,tag:str, class_name:str, html:str)->list:
+    return [i[attribute] for i in parse_tags_by_class(tag,class_name,html)]
