03add6193cc28f901f8fcbdf9cbf12800faf38e5
Joentze <Joentze01@gmail.com>
2022-11-19 01:01:37 +0800

    feat: added config type checking

--- playwright_types/config_types.py
+++ playwright_types/config_types.py
@@ -1,7 +1,18 @@
-# from typing import Dict
+from typing import Dict
 
-# PageImpressionType = Dict['str','object' or 'str']
-config_keys = []
+PageImpressionType = Dict['str','object' or 'str']
 
-def is_valid_config(config:PageImpressionType)->bool:
-    return len([i for i in config.keys if i in config_keys]) == len(config_keys)
+CLASS_CONFIG_KEYS = {"url":str, "tag":str, "class_name":str}
+
+def is_valid_config(config:PageImpressionType, config_type:object)->bool:
+    return len([i for i in config.keys() if i in config_type and config_type[i] == type(config[i])]) == len(config_type)
+
+test_config_obj = {
+    "url":"",
+    "tag":"",
+    "class_name":""
+}
+
+if __name__ == "__main__":
+    print(is_valid_config(test_config_obj))
+
