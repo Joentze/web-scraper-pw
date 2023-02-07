d6106bf67ba43fe48ac2f55ae136b021315f858d
Joen Tan <joentze01@gmail.com>
2023-02-07 18:16:10 +0800

    feat: added flask api app

--- croft_sub.py
+++ croft_sub.py
@@ -1,9 +1,16 @@
-from google.cloud import pubsub_v1
+# from google.cloud import pubsub_v1
 from pw_scrape import run_scraper
 from concurrent.futures import TimeoutError
-import ast
-import os
+# import ast
+# import os
 import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+PORT = int(os.environ["PORT"])
+
 # credentials_path = "/Users/tanjoen/Documents/web-scraper-pw/credentials/croft_pubsub_key.json"
 
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
@@ -33,3 +40,6 @@ while subscriber:
         streaming_pull_futures.result()
 
 print(future.result())
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=PORT)
